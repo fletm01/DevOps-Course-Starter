7fe9e2e4e71a0f914707d5d8b5c66babecf5a323
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-11-17 15:01:59 +0000

    Add items to Trello from Web Page

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, redirect, request
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items 
-from todo_app.data.session_items import add_item
+#from todo_app.data.session_items import add_item
+from todo_app.data.trello_items import add_item
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
