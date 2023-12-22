abd3b4bdb0f4d82baee9f2a08ce2ebb6313d36e1
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-12-22 11:42:41 +0000

    Add create app function

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,24 +3,25 @@ from todo_app.data.view_model import ViewModel
 from todo_app.flask_config import Config
 from todo_app.data.trello_items import add_item, get_items, move_item_to_done
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-app = Flask(__name__)
-app.config.from_object(Config())
+    @app.route('/')
+    def index():
+        items = get_items()
+        view_model = ViewModel(items)
+        return render_template('index.html', view_model = view_model)
 
+    @app.route('/add_new_items', methods=['POST'])
+    def add_new_item():
+        new_item_title = request.form.get('Title')
+        add_item (new_item_title)
+        return redirect('/')
 
-@app.route('/')
-def index():
-    items = get_items()
-    view_model = ViewModel(items)
-    return render_template('index.html', view_model = view_model)
+    @app.route('/complete-items/<todo_id>', methods=['POST'])
+    def complete_item(todo_id):
+        move_item_to_done(todo_id)
+        return redirect('/')
 
-@app.route('/add_new_items', methods=['POST'])
-def add_new_item():
-    new_item_title = request.form.get('Title')
-    add_item (new_item_title)
-    return redirect('/')
-
-@app.route('/complete-items/<todo_id>', methods=['POST'])
-def complete_item(todo_id):
-    move_item_to_done(todo_id)
-    return redirect('/')    
+    return app
