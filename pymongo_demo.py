61ecdd17dd7e2ca94706694258572834c886e44d
fletm01 Fletcher <michael.fletcher@asda.co.uk>
2024-08-12 16:17:18 +0000

    Script updated with addtions and updates

--- pymongo_demo.py
+++ pymongo_demo.py
@@ -6,4 +6,18 @@ dotenv.load_dotenv()
 
 client = pymongo.MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
 
-print(client.list_database_names())
+db = client.todoappdatabase
+
+collection = db.todo_items
+
+# Insert a record
+collection.insert_one({"description": "a pymongo document"})
+
+# Read all records
+list(collection.find())
+
+# Insert and update a record
+collection.insert_one({"description": "an updateable document", "type": "updateable"})
+collection.update_one({"type": "updateable"}, {"$set": {"description": "CHANGED!!!"}})
+
+print(collection)
